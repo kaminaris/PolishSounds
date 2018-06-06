1724e251abd67708f5a5635046e32e830406aee0
Pawel <zacdbg@gmail.com>
2018-06-06 22:10:49 +0200

    v0.4.0 - rebuild to amazon polly

--- build.php
+++ build.php
@@ -1,43 +1,85 @@
 <?php
 
-$fileList = array();
-$files = glob('Sounds/*.mp3');
+/*******************************************************************
+ * Config
+ *******************************************************************/
+
+$soundList = [
+	'Niebieski', 'Czerwony', 'Zielony', 'Żółty', 'Stack', 'Wypad', 'Czaszka', 'Cyc', 'Pomarańczowy', 'Purpurowy',
+	'Biały', 'Moon', 'Przerwij', 'Wypierdalaj', 'Lewo', 'Prawo', 'Środek', 'Pierwszy', 'Drugi', 'Trzeci',
+	'Czwarty', 'Piąty', 'Szósty', 'Uciekaj', 'Zabierz Bossa', 'Twoja Kolej', 'Stój', 'Odpałki',
+	'Addy', 'Rusz Się', 'Boss', 'Lewy Bok', 'Prawy Bok', 'Lewy Tył', 'Prawy Tył', 'Prawy Przód', 'Lewy Przód',
+	'Zabierz Adda', 'Przód', 'Tył', 'Bladlast', 'Heroizm', 'Aura', 'Bańka'
+];
+
+$language = 'PL'; //2 char code
+
+$amazonPollyConfig = [
+	'version' => 'latest',
+	'region' => 'eu-west-1', // Change this to your respective AWS region
+	'credentials' => [ // Change these to your respective AWS credentials
+		'key' => 'XXXXXXXXXXXXXXXXXXXX',
+		'secret' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+	]
+];
 
 
-foreach ($files as $file) {
-	$fileList[filectime($file)] = $file;
+/*******************************************************************
+ * Don't touch this
+ *******************************************************************/
+
+require(__DIR__.'/vendor/autoload.php');
+
+use Aws\Polly\PollyClient;
+
+// get service handle
+try {
+	$client = new PollyClient($amazonPollyConfig);
+} catch(Exception $e) {
+	print_r($e);
+	exit;
 }
+
+$downloadedSounds = [];
+$list = [];
+foreach ($soundList as $text) {
+	// get speech
+	$response = $client->synthesizeSpeech($text);
+	$sanitized = str_replace(' ', '_', transliterator_transliterate('Any-Latin; Latin-ASCII; Lower()', $text));
+
+	$fileName = "{$language}_{$sanitized}.mp3";
+	$fileLabel = $language . ' ' . $text;
+
+	file_put_contents("Sounds/{$fileName}", $response['AudioStream']);
+
+	$list[] = str_pad("{name = '{$fileLabel}',", 30) . " path = SoundPath .. '{$fileName}'}";
+
+	sleep(1);
+}
+
+
+$chunked = array_chunk($soundList, 5);
+$soundLine = [];
+foreach ($chunked as $sound) {
+	$soundLine[] = '-- ' . implode(', ', $sound);
+}
+
+/**
+ * Generating file
+ */
 
 function replaceBetween($content, $start, $stop, $replace) {
 	$pattern = '/' . ($start) . '(.*)' . ($stop) .'/s';
 	return preg_replace($pattern, $start . "\n" . $replace . "\n" . $stop, $content);
 }
 
-ksort($fileList);
-
-$list = array();
-$i = 0;
-$soundList = array();
-foreach ($fileList as $file) {
-	preg_match('/\/([\w]+)\./', $file, $matches);
-	$l0 = str_pad('[' . (++$i) . ']', 5);
-	$l1 = str_pad('{name = "PL ' . $matches[1] . '",', 30);
-	$list[] = '	' . $l0 . ' = ' . $l1 . ' path = SoundPath .. "' . $matches[1] . '.mp3"}';
-	$soundList[] = $matches[1];
-}
-
-$listImploded = implode(",\n", $list);
-$chunked = array_chunk($soundList, 5);
-$soundLine = array();
-foreach ($chunked as $sound) {
-	$soundLine[] = '-- ' . implode(', ', $sound);
-}
 
 $content = file_get_contents('core.lua');
 
-
-$content = replaceBetween($content, '-- start --', '-- end --', $listImploded);
+$content = replaceBetween($content, '-- start --', '-- end --', implode(",\n", $list));
 $content = replaceBetween($content, '-- soundlist --', '-- soundlistend --', implode("\n", $soundLine));
 //echo $content;
+
 file_put_contents('core.lua', $content);
+
 echo implode(', ', $soundList);
